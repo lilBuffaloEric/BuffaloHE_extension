2559de2726a93545f133f163d27a60ed52d446ef
lilBuffaloEric <ericde1920@foxmail.com>
2023-08-08 01:31:31 +0800

    add instruction comments to check.go

--- check.go
+++ check.go
@@ -3,18 +3,19 @@ package main
 import "project1-fhe_extension_v1.0/check"
 
 func main() {
-	// var debug = true
+	// ------------------------------ Internal Components Check (Not all available) ------------------------------
 	// check.CovMatrix_check()
 	// check.MatrixMultX_check(true)
 	// check.LinTransX_check()
-	check.LinTransX2_check()
+	// check.LinTransX2_check()
+	// check.LinTransX3_check()
 	// check.Transepose_check()
 	// check.OnlyMatrixMult_check()
-	//check.Invsqrt_check() // checked
+	// check.Invsqrt_check() // checked
 	// check.SquareMatrix_product_check() // checked
 	// check.SquareMatrix_encode_check() // checked
 	// check.SquareMatrix_permute_check() // checked
-	//check.Piecewise_check() // checked
+	// check.Piecewise_check() // checked
 	// check.Softmax_check() // checked
 	// check.Csvio_check()
 	// check.PCA_check(false) // 202356: update N1 as 32 for all Lintrans.
@@ -25,8 +26,13 @@ func main() {
 	// check.Correctness_check()
 	// check.ErrorCtrlX_check(true)
 	// check.StoreAndLoadKeys_btpVersion_check(true)
-	//check.PCA_btpVersion_check(true, true)
-	// check.CovMatrix_btpVersion_check(true, false)
+
+	// ------------------------------ Available for testing ------------------------------
+	// check.LinTransX3_check() // Routine Testing the Diagonal Convergence Decomposition of the Lintrans in homomorphic matrix multiplication.
+	// check.PCA_btpVersion_check(false, false) // Routine Computing Cov' = 1/N*X^T*X and U = u*u^T
+	check.PCA_largeSpace_Ver2_check(false) // Routine Computing Cov' = 1/N*X^T*X and U = u*u^T (theoratically faster, but need more space to cache intermediate homomorphic matrix-mult results in memory)
+	// check.CovMatrix_btpVersion_check(false, false) // Routine Computing Cov = Cov' - U and the approximate eigenvectors of Cov. Use bootstrpping if reencryptionMode is set to "Ture", but correctness cannot be guaranteed.
+	// check.PandaPCA_check(true) // Routine simulating the Panda's scheme (2021). This can only be reguarded as a speed testing, and no correctness can be guaranteed.
 }
 
 /*
